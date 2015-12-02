6d95518535d9f5705bee634a80502239a81f0889
Casey Megginson <cemegginson@ualr.edu>
2015-12-02 15:23:40 -0600

    Removed macro shenanigans and added missing include.

--- source/art_library/ArtAssetLibrary.cpp
+++ source/art_library/ArtAssetLibrary.cpp
@@ -1,46 +1,43 @@
 #include <string>
+#include <utility>
 
 #include "art_library/ArtAssetLibrary.h"
 
 ArtAssetLibrary::ArtAssetLibrary() {}
 
 ArtAssetLibrary::~ArtAssetLibrary() {
-    for(auto iter = library_.begin(); iter!=library_.end();) {
+    for(auto iter = library_.begin(); iter != library_.end();) {
         delete iter->second;
         iter = library_.erase(iter);
     }
 }
 
 bool ArtAssetLibrary::LoadAssets(Renderer* renderer, SDLView* SDLView) {
-    #if RENDER_TYPE == 0
-        SDL_Renderer* tmp_renderer = static_cast<SDLRenderer*>(renderer)->renderer();
-        // Bullet
-        library_.insert(std::pair<std::string, SDLTexture*>("Bullet", new SDLTexture()));
-        std::string bullet_path = "./Assets/Images/t_bullet.png";
-        library_.at("Bullet")->Initialize(tmp_renderer, SDLView, bullet_path);
+    SDL_Renderer* tmp_renderer = static_cast<SDLRenderer*>(renderer)->renderer();
+    // Bullet
+    library_.insert(std::pair<std::string, SDLTexture*>("Bullet", new SDLTexture()));
+    std::string bullet_path = "./Assets/Images/t_bullet.png";
+    library_.at("Bullet")->Initialize(tmp_renderer, SDLView, bullet_path);
 
-        // Carrier
-        library_.insert(std::pair<std::string, SDLTexture*>("Carrier", new SDLTexture()));
-        std::string carrier_path = "./Assets/Images/t_carrier.png";
-        library_.at("Carrier")->Initialize(tmp_renderer, SDLView, carrier_path);
+    // Carrier
+    library_.insert(std::pair<std::string, SDLTexture*>("Carrier", new SDLTexture()));
+    std::string carrier_path = "./Assets/Images/t_carrier.png";
+    library_.at("Carrier")->Initialize(tmp_renderer, SDLView, carrier_path);
 
-        // Infantry
-        library_.insert(std::pair<std::string, SDLTexture*>("Infantry", new SDLTexture()));
-        std::string infantry_path = "./Assets/Images/t_infantry.png";
-        library_.at("Infantry")->Initialize(tmp_renderer, SDLView, infantry_path);
+    // Infantry
+    library_.insert(std::pair<std::string, SDLTexture*>("Infantry", new SDLTexture()));
+    std::string infantry_path = "./Assets/Images/t_infantry.png";
+    library_.at("Infantry")->Initialize(tmp_renderer, SDLView, infantry_path);
 
-        // Samus
-        library_.insert(std::pair<std::string, SDLTexture*>("Samus", new SDLTexture()));
-        std::string player_path = "./Assets/Images/samus.gif";
-        library_.at("Samus")->Initialize(tmp_renderer, SDLView, player_path);
+    // Samus
+    library_.insert(std::pair<std::string, SDLTexture*>("Samus", new SDLTexture()));
+    std::string player_path = "./Assets/Images/samus.gif";
+    library_.at("Samus")->Initialize(tmp_renderer, SDLView, player_path);
 
-        // Rock
-        library_.insert(std::pair<std::string, SDLTexture*>("Rock", new SDLTexture()));
-        std::string rock_path = "./Assets/Images/t_rock.png";
-        library_.at("Rock")->Initialize(tmp_renderer, SDLView, rock_path);
-    #endif
-
-    // I don't know what to do with loading to OpenGL stuff
+    // Rock
+    library_.insert(std::pair<std::string, SDLTexture*>("Rock", new SDLTexture()));
+    std::string rock_path = "./Assets/Images/t_rock.png";
+    library_.at("Rock")->Initialize(tmp_renderer, SDLView, rock_path);
 
     return true;
 }
