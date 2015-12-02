36211b26b584a644f38557d22fd455c34fdbdf3c
Casey Megginson <cemegginson@ualr.edu>
2015-12-02 15:21:04 -0600

    Fixed missing include.

--- source/render/opengl/opengl_renderer.cpp
+++ source/render/opengl/opengl_renderer.cpp
@@ -1,26 +1,27 @@
 #include "render/opengl/opengl_renderer.h"
+#include "util/game_utils.h"
 
 OpenGLRenderer::OpenGLRenderer() {
     window_ = nullptr;
-    //renderer_ = nullptr;
+    // renderer_ = nullptr;
     width_ = 0;
     height_ = 0;
 }
 
 OpenGLRenderer::OpenGLRenderer(uint32 screen_width, uint32 screen_height) {
     window_ = nullptr;
-    //renderer_ = nullptr;
+    // renderer_ = nullptr;
     width_ = screen_width;
     height_ = screen_height;
 }
 
 OpenGLRenderer::~OpenGLRenderer() {
-    //SDL_DestroyWindow(window_);
+    // SDL_DestroyWindow(window_);
 }
 
 bool OpenGLRenderer::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        //LogSDLError(std::cerr, "SDL_Init");
+        LogSDLError(std::cerr, "SDL_Init");
         return false;
     }
 
@@ -33,7 +34,7 @@ bool OpenGLRenderer::Initialize() {
     );
 
     if (window_ == nullptr) {
-        //LogSDLError(std::cerr, "SDL_CreateWindow");
+        LogSDLError(std::cerr, "SDL_CreateWindow");
         return false;
     }
 
