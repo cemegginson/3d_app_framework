f3bd81cf936e070962b5c1c1f01c1418cda4ebcd
Casey Megginson <cemegginson@ualr.edu>
2016-01-04 17:02:33 -0600

    Cleaned up function calls but still can't figure out why it won't draw anything.

--- source/render/opengl/gl_model.cpp
+++ source/render/opengl/gl_model.cpp
@@ -38,15 +38,7 @@ void GlModel::Draw(glm::mat4 model_view_projection) {
     // 2nd attribute buffer : colors
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, color_buffer_object_);
-    glVertexAttribPointer(
-        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        3,                                // size
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-    );
-
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 }
 
 void GlModel::BufferData() {
